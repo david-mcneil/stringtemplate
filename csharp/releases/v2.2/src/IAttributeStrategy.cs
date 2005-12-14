d8d78b433021174b09a024fb720c3e6228e99c53
michael <open.zone@virgin.net>
2005-12-14 06:20:36 -0800

    More missing ST# 2.2 files
    [git-p4: depot-paths = "//depot/code/stringtemplate/": change = 2147]

---
+++ csharp/releases/v2.2/src/IAttributeStrategy.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace antlr.stringtemplate
+{
+	/// <summary>
+	/// Summary description for IAttributeStrategy.
+	/// </summary>
+	public interface IAttributeStrategy
+	{
+		bool testAttributeTrue(Object a);
+	}
+}
